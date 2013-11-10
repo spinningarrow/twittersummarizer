fcaf40f344b2a16d23fea41832c80c3ffb1301d6
Sahil Bajaj <sahil29@gmail.com>
2013-11-10 19:05:49 +0800

    Add basic web app

--- web.py
+++ web.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from subprocess import check_output
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello world!"
+	return render_template("layout.html")
 
-@app.route("/java")
+@app.route("/java", methods=['POST'])
 def java():
 	result = check_output(["java", "-cp", "'*'", "-jar", "TwitterNLP.jar", "data/SampleSet1_POS.txt", "models/SerializedModel4"])
 	# return Response(dumps(result), status=200, mimetype='application/json')
