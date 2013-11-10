b7b23f62b5a9edd41e0858f6294b739cab5a865c
Sahil Bajaj <sahil29@gmail.com>
2013-11-10 19:29:29 +0800

    Show positive and negative results

--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,32 @@ $(document).ready(function () {
 		}
 
 		// Send the query to the Java method
-		$.post('/java', {
+		$.post('/javamock', {
 			query: query
-		}, function (data) {
-			$('#query-results').html(data);
+		}, function (data, status, xhr) {
+
+			if (status !== "success") {
+				return
+			}
+
+			// Convert JSON result to a JS object
+			var resultObject = $.parseJSON(data)
+
+			// Loop through the positive and negative results and add them
+			// to the HTML
+			var $ul = $('<ul></ul>').appendTo("#query-results-positive")
+
+			for (var i = 0; i < resultObject.positive.length; i++) {
+				$ul.append("<li>" + resultObject.positive[i] + "</li>")
+			}
+
+			$ul = $('<ul></ul>').appendTo("#query-results-negative")
+
+			for (var i = 0; i < resultObject.negative.length; i++) {
+				$ul.append("<li>" + resultObject.negative[i] + "</li>")
+			}
+
+			// $('#query-results-positive').html(data);
 		})
 	})
 })
--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 from subprocess import check_output
 app = Flask(__name__)
 
@@ -11,6 +11,27 @@ def java():
 	result = check_output(["java", "-cp", "'*'", "-jar", "TwitterNLP.jar", "data/SampleSet1_POS.txt", "models/SerializedModel4"])
 	# return Response(dumps(result), status=200, mimetype='application/json')
 	return result
+
+# Return mock data for styling web app properly
+@app.route("/javamock", methods=['POST'])
+def javamock():
+	result = {
+		"positive": [
+			"I love it",
+			"It's amazing",
+			"Awesome!",
+			"So thin!",
+			"So light!",
+			"Perfect!"
+		],
+		"negative": [
+			"Where's the bezel?",
+			"So expensive",
+			"Google is better",
+			"I hate it"
+		]
+	}
+	return json.dumps(result)
 
 if __name__ == "__main__":
 	app.run()
