b59a5a3f3e0181e04ac8cde0242e7ca642fc8af8
Sahil Bajaj <sahil29@gmail.com>
2013-11-16 19:48:17 +0800

    Revert "Using workers"
    This reverts commit 65c2bfc733ba6d3398208cfd57773984445022b1.

--- web.py
+++ web.py
@@ -1,12 +1,6 @@
 from flask import Flask, json, render_template, request, Response
 from subprocess import check_output
 import analyser
-from rq import Queue, get_current_job
-from rq.job import Job
-from worker import conn
-from utils import get_describing_phrases
-
-q = Queue(connection=conn)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,19 +12,20 @@ def hello():
 @app.route("/java", methods=['POST', 'GET']) # remove GET later
 def java():
 	query = request.form['query']
-	job = q.enqueue(get_describing_phrases, query)
+	result = check_output(["java", "-jar", "TwitterNLP.jar", "data/database.db", query])
+	# return Response(dumps(result), status=200, mimetype='application/json')
+	app.logger.debug('Finished Java work, moving on to sentiment analysis...')
 
-	return job.key
+	result_list = result.split("\n")
+	classifications = analyser.getClassifiedDictionary(result_list)
+	# classifications = {
+	# 	'positive': result_list[:10],
+	# 	'negative': result_list[10:20]
+	# }
 
-@app.route("/java_result/<job_key>", methods=['GET'])
-def java_result(job_key):
-	job_key = job_key.replace("rq:job:", "")
-	job = Job.fetch(job_key, connection=conn)
+	app.logger.debug('Finished sentiment analysis.')
 
-	if(not job.is_finished):
-		return "Not yet", 202
-	else:
-		return str(job.result), 200
+	return json.dumps(classifications)
 
 # Return mock data for styling web app properly
 @app.route("/javamock", methods=['POST'])
